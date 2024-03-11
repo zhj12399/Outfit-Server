847d9ba93f3e8f8f822bf46ee6c38cafe1adc069
Haojie Zhang <zhj727534681@163.com>
2024-03-11 10:26:23 +0800

    2024-3-11

---
+++ src/test/java/cn/zhj12399/outfit/OutfitApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhj12399.outfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
